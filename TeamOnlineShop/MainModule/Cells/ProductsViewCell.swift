90260606e637ad604027b5d2d995d21f1ff617ee
Даниил Мурзин <danmur92@mail.ru>
2024-04-16 00:10:52 +0500

    MVP module added

---
+++ TeamOnlineShop/MainModule/Cells/ProductsViewCell.swift
@@ -0,0 +1,123 @@
+import UIKit
+
+class ProductsViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static var reuseIdentifier: String {"\(Self.self)"}
+    
+    // MARK: - UI
+    private lazy var imageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "CellImage")
+        element.contentMode = .scaleAspectFit
+        element.clipsToBounds = true
+        element.tintColor = UIColor(named: "TintColor")
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var productNameLabel: UILabel = {
+        let element = UILabel()
+        element.text = "Monitor LG 22”inc 4K 120Fps"
+        element.numberOfLines = 1
+        element.font = UIFont(name: "Inter-Regular", size: 12)
+        element.tintColor = UIColor(named: "TintColor")
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let element = UILabel()
+        element.text = "$199.99"
+        element.font = UIFont(name: "Inter-SemiBold", size: 14)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var addToCartButton: UIButton = {
+        let element = UIButton(type: .system)
+
+        element.setTitle("Add to cart", for: .normal)
+        element.titleLabel?.font = UIFont(name: "Inter-Regular", size: 12)
+        element.setTitleColor(.white, for: .normal)
+        element.backgroundColor = UIColor(named: "ButtonColor")
+        element.layer.cornerRadius = 4
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var VStack: UIStackView = {
+        let element = UIStackView()
+        
+        element.axis = .vertical
+        element.spacing = 13
+        element.alignment = .fill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private funcs
+    private func setupView(){
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(VStack)
+        
+        [productNameLabel,
+         priceLabel,
+         addToCartButton
+        ].forEach { VStack.addArrangedSubview($0)}
+        
+        contentView.backgroundColor = UIColor(named: "ButtonBackground")
+        contentView.layer.cornerRadius = 15
+    }
+    
+    private func setupConstraints(){
+        
+        NSLayoutConstraint.activate([
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            
+            VStack.topAnchor.constraint(
+                equalTo: imageView.bottomAnchor,
+                constant: 13
+            ),
+            VStack.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor,
+                constant: -13
+            ),
+            VStack.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor,
+                constant: 13),
+            VStack.trailingAnchor.constraint(
+                equalTo: contentView.trailingAnchor,
+                constant: -13
+            ),
+            
+            
+            addToCartButton.heightAnchor.constraint(equalToConstant: 31),
+            addToCartButton.widthAnchor.constraint(equalToConstant: 144)
+        ])
+    }
+}
+
+
+
+
+
+
+
--- TeamOnlineShop/MainModule/MainViewController.swift
+++ TeamOnlineShop/MainModule/MainViewController.swift
@@ -6,12 +6,54 @@ class MainViewController: UIViewController {
     // MARK: - Properties
      var presenter: MainPresenterProtocol!
     
+    // MARK: - UI
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 170, height: 230)
+        layout.minimumInteritemSpacing = 7
+        layout.minimumLineSpacing = 7
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ProductsViewCell.self, forCellWithReuseIdentifier: ProductsViewCell.reuseIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor =  .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        setupCollectionViewConstraints()
+    }
+    
+    private func setupCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -15),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 30),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -15)
+        ])
+    }
+
+}
+// MARK: - MainViewController + MainViewProtocol
+extension MainViewController: MainViewProtocol {}
+
+// MARK: - MainViewController + UICollectionViewDataSource
+extension MainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductsViewCell.reuseIdentifier, for: indexPath) as! ProductsViewCell
+        return cell
     }
 }
 
-extension MainViewController: MainViewProtocol {}
+extension MainViewController: UICollectionViewDelegate {}
