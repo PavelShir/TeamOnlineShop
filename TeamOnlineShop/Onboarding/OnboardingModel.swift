6980021afc5751117e9d21d92ac874a85645411c
Pavel Shirokiy <pshirokiy@gmail.com>
2024-04-16 02:18:44 +0300

    add Onboarding view, Cell and its Model, start some logic

---
+++ TeamOnlineShop/Onboarding/OnboardingModel.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingModel.swift
+//  TeamOnlineShop
+//
+//  Created by Павел Широкий on 16.04.2024.
+//
+
+import UIKit
+
+struct OnboardingModel {
+    
+    let image: UIImage
+    let firstLabel: String
+    let secondLabel: String
+    
+}
