7668de7bad2395d974d0caae0615ae38d790643e
Maksim Stogniy <mstogniy@ya.ru>
2024-04-20 15:32:32 +0300

    Add tabbar

--- TeamOnlineShop/SceneDelegate.swift
+++ TeamOnlineShop/SceneDelegate.swift
@@ -14,11 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let mainViewController = MainModuleBuilder.build()
         let window = UIWindow(windowScene: windowScene)
-
-        window.rootViewController = UINavigationController(rootViewController: OnboardingViewController())
+        
+        let tabBarVC = TabBarController()
+        window.rootViewController = tabBarVC
 
         window.makeKeyAndVisible()
         
