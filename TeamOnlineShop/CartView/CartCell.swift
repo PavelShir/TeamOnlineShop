c855517c0faee2670758fd732091c8afb75debcb
s9888556 <s9888556@yandex.ru>
2024-04-20 15:28:32 +0500

    add ViewCollection cartView

--- TeamOnlineShop/CartView/CartCell.swift
+++ TeamOnlineShop/CartView/CartCell.swift
@@ -9,4 +9,150 @@ import UIKit
 
 class CartCell: UICollectionViewCell {
     
+    private lazy var cellStack: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.spacing = 10
+//        element.distribution = .equalSpacing
+        element.backgroundColor = .white
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var checkButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal) // пустой "square"
+        element.tintColor = .greenPrimary
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let element = UIImageView()
+        element.contentMode = .scaleAspectFill
+        element.layer.cornerRadius = 5
+        element.clipsToBounds = true
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var detailsStack: UIStackView = {
+        let element = UIStackView()
+        element.axis = .vertical
+        element.distribution = .equalSpacing
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.font = UIFont.TextFont.Screens.ShopCartItem.title
+        element.tintColor = .blackLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let element = UILabel()
+        element.font = UIFont.TextFont.Screens.ShopCartItem.price
+        element.tintColor = .blackLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var buttonStack: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.spacing = 5
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var minusButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage.Icons.minus, for: .normal)
+        element.tintColor = .greyLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var numberLabel: UILabel = {
+        let element = UILabel()
+        element.tintColor = .blackLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage.Icons.plus, for: .normal)
+        element.tintColor = .greyLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var trashButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage.Icons.trashCircle, for: .normal)
+        element.tintColor = .greyLight
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    func setup(_ item: CartModel) {
+        
+        imageView.image = UIImage(named: "CellImage")
+        titleLabel.text = item.title
+        priceLabel.text = "$ " + String(item.price)
+        numberLabel.text = String(item.amount)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupConstraints() {
+        self.contentView.addSubview(cellStack)
+        cellStack.addArrangedSubview(checkButton)
+        cellStack.addArrangedSubview(imageView)
+        cellStack.addArrangedSubview(detailsStack)
+
+        detailsStack.addArrangedSubview(titleLabel)
+        detailsStack.addArrangedSubview(priceLabel)
+        
+        
+        detailsStack.addSubview(buttonStack)
+        buttonStack.addArrangedSubview(minusButton)
+        buttonStack.addArrangedSubview(numberLabel)
+        buttonStack.addArrangedSubview(plusButton)
+        buttonStack.addArrangedSubview(trashButton)
+        
+        self.clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            
+            cellStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            cellStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            cellStack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            cellStack.heightAnchor.constraint(equalToConstant: 80),
+            
+            checkButton.heightAnchor.constraint(equalToConstant: 25),
+            checkButton.widthAnchor.constraint(equalToConstant: 25),
+            
+            imageView.heightAnchor.constraint(equalToConstant: 80),
+            imageView.widthAnchor.constraint(equalToConstant: 80),
+            
+            detailsStack.topAnchor.constraint(equalTo: cellStack.topAnchor, constant: 5),
+            detailsStack.bottomAnchor.constraint(equalTo: cellStack.bottomAnchor, constant: -5),
+            
+            buttonStack.bottomAnchor.constraint(equalTo: cellStack.bottomAnchor),
+            buttonStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5)
+        ])
+    }
 }
--- TeamOnlineShop/CartView/CartViewController.swift
+++ TeamOnlineShop/CartView/CartViewController.swift
@@ -9,8 +9,57 @@ import UIKit
 
 class CartViewController: UIViewController {
     
+    let itemCart: [CartModel] = Cart.allItems()
+    
+    //MARK: - Set Navigation Bar
+    
+    private lazy var backButton: UIBarButtonItem = {
+        let element = UIBarButtonItem()
+        element.image = .Icons.arrowLeft
+        element.tintColor = .blackLight
+//        element.action = #selector(backButtonTapped)
+        element.target = self
+        return element
+    }()
+    
+    private lazy var cartButton: UIBarButtonItem = {
+        let element = UIBarButtonItem()
+        element.image = .Icons.cart
+        element.tintColor = .blackLight
+//        element.action = #selector(cartButtonTapped)
+        return element
+    }()
+    
+    private lazy var countCart: UILabel = {
+        let element = UILabel()
+
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private func setNavigationBar() {
+        title = "Your Cart"
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItem = cartButton
+
+    }
+    
     //MARK: - UI
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = .init(width: 350, height: 80)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(CartCell.self, forCellWithReuseIdentifier: "CartCell")
+        collectionView.dataSource = self
+//        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+//        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isUserInteractionEnabled = false
+        return collectionView
+    }()
     
     
     //MARK: - LifeCycle
@@ -18,6 +67,7 @@ class CartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setNavigationBar()
         setViews()
         setupConstraints()
     }
@@ -25,8 +75,11 @@ class CartViewController: UIViewController {
     //MARK: - Set Views
     
     private func setViews() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        
+        view.addSubview(collectionView)
     }
+    
 }
 
 //MARK: - Setup Constraints
@@ -34,12 +87,28 @@ class CartViewController: UIViewController {
 extension CartViewController {
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 15),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
+        ])
     }
 }
 
-#Preview {
-    var controller = CartViewController()
+//MARK: - Delegate
+
+extension CartViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(itemCart.count)
+        return itemCart.count
+    }
     
-    return controller
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CartCell", for: indexPath) as! CartCell
+        cell.setup(Cart.allItems()[indexPath.item])
+        return cell
+    }
+    
+    
 }
