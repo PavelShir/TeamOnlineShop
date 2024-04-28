67bfe58e85ae2ad3db6c9d8f604a7ccaa3c21733
Pavel Shirokiy <pshirokiy@gmail.com>
2024-04-28 20:26:49 +0300

    change SceneDelegate, add Auth check, add save User to FB, add Sing out on Profile

--- TeamOnlineShop/Models/User.swift
+++ TeamOnlineShop/Models/User.swift
@@ -22,7 +22,6 @@ struct User {
     let id: String
     var username: String = ""
     var email: String = ""
-    var pass: String = ""
     var image: String?
     var type: String = UserType.user.rawValue
     var cart: [Product] = []
